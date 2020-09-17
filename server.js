561540898d39a8dcbbaefca4ba1dd34f2c962ce9
Adrian Frunza <adrian.frunza@softvision.ro>
2020-09-18 02:20:29 +0300

    Little bit of cleanup

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ import express from "express";
 import { config } from "./config.js";
 import fs from "fs";
 import { mapTitleToKebabCase } from "./utils.js";
-import fileUpload from "express-fileupload";
 
 // probably converting from markdown to html is complicated, so I didn't want to reinvent the wheel, I just used this library that I found
 import showdown from "showdown";
+import { initApp } from "./initApp.js";
 const converter = new showdown.Converter();
-
-const app = express();
 
 const {
   port,
@@ -18,8 +16,17 @@ const {
   NOT_FOUND_ROUTE,
   NOT_FOUND_ROUTE_MESSAGE,
   POSTS_ROUTE,
-  ADD_ROUTE
+  ADD_ROUTE,
 } = config;
+
+const app = express();
+initApp(app);
+
+// here I watch for any added files to the folder. If a new file is added to the folder, the articles object will be updated
+fs.watch(ARTICLES_PATH, (eventType, fileName) => {
+  articles[mapTitleToKebabCase(fileName)] = fileName;
+});
+
 // articles is the object where we keep the mapping between the original file name and the kebab-case one
 const articles = {};
 
@@ -39,35 +46,41 @@ const readFiles = (() => {
   });
 })();
 
+// this is getting all the posts which are inserted into the ejs template
 const getAllPosts = (res) => {
   fs.readdir(ARTICLES_PATH, (err, files) => {
-    const html = `<html>${files.map(
-      (file) =>
-        `<li><a href="/blog/${mapTitleToKebabCase(file)}">${file}</a></li>`
-    )}</html>`;
-    res.send(err || html);
+    if (error) {
+      res.send(error);
+    } else {
+      res.render("articles", {
+        root: "./",
+        files: files.map((file) => mapTitleToKebabCase(file)),
+      });
+    }
+    resetError();
   });
+};
+
+//this will render the file
+const getPost = (title, res) => {
+  if (articles[title]) {
+    const t = fs.readFile(
+      `${ARTICLES_PATH}/${articles[title]}`,
+      (err, data) => {
+        error = err || error;
+        res.send(error || converter.makeHtml(data.toString()));
+        resetError();
+      }
+    );
+  } else {
+    res.redirect(NOT_FOUND_ROUTE);
+  }
+  resetError();
 };
 
 const resetError = () => {
   error = null;
 };
-
-// here I watch for any added files to the folder. If a new file is added to the folder, the articles object will be updated
-fs.watch(ARTICLES_PATH, (eventType, fileName) => {
-  articles[mapTitleToKebabCase(fileName)] = fileName;
-});
-
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store");
-  next();
-});
-
-app.use(
-  fileUpload({
-    createParentPath: true,
-  })
-);
 
 app.get(DEFAULT_ROUTE, (req, res) => {
   if (error) {
@@ -104,22 +117,6 @@ app.get(POSTS_ROUTE, (req, res) => {
 app.get(NOT_FOUND_ROUTE, (req, res) => {
   res.send(error || NOT_FOUND_ROUTE_MESSAGE);
 });
-
-const getPost = (title, res) => {
-  if (articles[title]) {
-    const t = fs.readFile(
-      `${ARTICLES_PATH}/${articles[title]}`,
-      (err, data) => {
-        error = err || error;
-        res.send(error || converter.makeHtml(data.toString()));
-        resetError();
-      }
-    );
-  } else {
-    res.redirect(NOT_FOUND_ROUTE);
-  }
-  resetError();
-};
 
 app.get(BLOG_ROUTE, (req, res) => {
   getPost(req.params.title.toLowerCase(), res);
