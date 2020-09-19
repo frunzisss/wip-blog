e8bbbf0b76559ab8b8c9a514697a6a8b27ed2db6
Adrian Frunza <adrian.frunza@softvision.ro>
2020-09-19 14:47:57 +0300

    Format files

--- initApp.js
+++ initApp.js
@@ -1,21 +1,21 @@
 import fileUpload from "express-fileupload";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
 export const initApp = (app) => {
-    app.use((req, res, next) => {
-        res.set("Cache-Control", "no-store");
-        next();
-      });
-      
-      app.use(
-        fileUpload({
-          createParentPath: true,
-        })
-      );
-      
-      app.set("view engine", "ejs"); 
-      
-      app.set("views", "./views"); 
-      
-      app.use(bodyParser.urlencoded({ extended: false })); 
+  app.use((req, res, next) => {
+    res.set("Cache-Control", "no-store");
+    next();
+  });
+
+  app.use(
+    fileUpload({
+      createParentPath: true,
+    })
+  );
+
+  app.set("view engine", "ejs");
+
+  app.set("views", "./views");
+
+  app.use(bodyParser.urlencoded({ extended: false }));
 };
--- utils.js
+++ utils.js
@@ -3,4 +3,9 @@
 // 2. replace any spaces with dashes
 // 3 .remove characters that are not alphanumeric or -._
 // 4. lowercase it
-export const mapTitleToKebabCase = (string) => string.replace(/\.[^/.]+$/, "").replace(/ /g, '-').replace(/[^A-Z0-9-._]/ig, "").toLowerCase();
+export const mapTitleToKebabCase = (string) =>
+  string
+    .replace(/\.[^/.]+$/, "")
+    .replace(/ /g, "-")
+    .replace(/[^A-Z0-9-._]/gi, "")
+    .toLowerCase();
